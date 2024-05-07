940cc0d2b3edb7aa8e9d852f6a2241db5f546724
Artem <artemdem972@gmail.com>
2024-05-07 20:55:13 +0300

    upd

--- SteamStorage/Utilities/UrlUtility.cs
+++ SteamStorage/Utilities/UrlUtility.cs
@@ -3,25 +3,24 @@ using System.Runtime.InteropServices;
 
 namespace SteamStorage.Utilities;
 
-public static class UrlUtility
+internal static class UrlUtility
 {
     internal static void OpenUrl(string url)
     {
-        Process.Start(url);
-        // if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-        // {
-        //     Process.Start(new ProcessStartInfo(url)
-        //     {
-        //         UseShellExecute = true
-        //     });
-        // }
-        // else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-        // {
-        //     Process.Start("xdg-open", url);
-        // }
-        // else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-        // {
-        //     Process.Start("open", url);
-        // }
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            Process.Start(new ProcessStartInfo(url)
+            {
+                UseShellExecute = true
+            });
+        }
+        else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+        {
+            Process.Start("xdg-open", url);
+        }
+        else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+        {
+            Process.Start("open", url);
+        }
     }
 }
