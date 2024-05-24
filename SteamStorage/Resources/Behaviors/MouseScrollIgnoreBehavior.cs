c5b1a1a7108d0d19c3ba4cec6b21c7841ed60520
Artem <artemdem972@gmail.com>
2024-05-24 16:32:17 +0300

    [+] global scroll disabled while pointer over cartesian chart

---
+++ SteamStorage/Resources/Behaviors/MouseScrollIgnoreBehavior.cs
@@ -0,0 +1,30 @@
+﻿using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Xaml.Interactivity;
+
+namespace SteamStorage.Resources.Behaviors;
+
+public class MouseScrollIgnoreBehavior : Behavior<Control>
+{
+    #region Methods
+
+    protected override void OnAttached()
+    {
+        base.OnAttached();
+        AssociatedObject?.AddHandler(InputElement.PointerWheelChangedEvent, PointerWheelChangedHandler, RoutingStrategies.Bubble);
+    }
+
+    protected override void OnDetaching()
+    {
+        base.OnDetaching();
+        AssociatedObject?.RemoveHandler(InputElement.PointerWheelChangedEvent, PointerWheelChangedHandler);
+    }
+
+    private void PointerWheelChangedHandler(object? sender, PointerWheelEventArgs e)
+    {
+        e.Handled = true;
+    }
+
+    #endregion Methods
+}
